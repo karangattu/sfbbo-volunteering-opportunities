6c8d6d445bf504822e3bd70c3df41f686d6013aa
Karan Gathani <karan.gathani@posit.co>
2026-04-24 18:17:02 -0700

    Update icons and bump service-worker cache to v19
    Replace favicon.svg with a Lucide bird SVG (credited CC BY 4.0) and update app icons (icon-192.png, icon-512.png) plus assets/AVIAN_DISEASE_PREVENTION_PROGRAM_HOW.png. Bump CACHE_NAME in service-worker.js from v18 to v19 to force clients to refresh cached assets and pick up the updated icons.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sfbbo-volunteer-explorer-v18";
+const CACHE_NAME = "sfbbo-volunteer-explorer-v19";
 const BASE_PATH = self.location.pathname.replace(/service-worker\.js$/, "");
 const PROGRAM_SLUGS = [
   "colonial-waterbird-nest-monitoring",
